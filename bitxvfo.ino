be738504eb7cce83d3d6f3320f23c048a094b3ca
Todd <todor.galabov@artirix.com>
2015-11-27 23:32:33 +0200

    modified:   bitxvfo.ino

--- bitxvfo.ino
+++ bitxvfo.ino
@@ -4,13 +4,24 @@
 #include <AD9850.h>
 #include <Encoder.h>
 
+const int SCLK_PIN = 9;
+const int DIN_PIN = 8;
+const int DC_PIN = 7;
+const int CS_PIN = 6;
+const int RST_PIN = 5;
+
+const int W_CLK_PIN = 10;
+const int FQ_UD_PIN = 11;
+const int DATA_PIN = 12;
+const int RESET_PIN = 13;
+
 // Software SPI (slower updates, more flexible pin options):
 // pin 7 - Serial clock out (SCLK)
 // pin 6 - Serial data out (DIN)
 // pin 5 - Data/Command select (D/C)
 // pin 4 - LCD chip select (CS)
 // pin 3 - LCD reset (RST)
-Adafruit_PCD8544 display = Adafruit_PCD8544(7, 6, 5, 4, 12);
+Adafruit_PCD8544 display = Adafruit_PCD8544(SCLK_PIN, DIN_PIN, DC_PIN, CS_PIN, RST_PIN);
 
 // Hardware SPI (faster, but must use certain hardware pins):
 // SCK is LCD serial clock (SCLK) - this is pin 13 on Arduino Uno
@@ -22,7 +33,7 @@ Adafruit_PCD8544 display = Adafruit_PCD8544(7, 6, 5, 4, 12);
 // Note with hardware SPI MISO and SS pins aren't used but will still be read
 // and written to during SPI transfer.  Be careful sharing these pins!
 
-AD9850 ad(8, 9, 10); // w_clk, fq_ud, d7
+AD9850 ad(W_CLK_PIN, FQ_UD_PIN, DATA_PIN); // w_clk, fq_ud, d7
 
 // Change these two numbers to the pins connected to your encoder.
 //   Best Performance: both pins have interrupt capability
